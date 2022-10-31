2de5e081638d7c33953b7bbd2c0d9f3900e0420f
MuhammadHumam <91688143+Humam1234@users.noreply.github.com>
2022-10-31 09:10:31 +0700

    Add files via upload

--- MODUL1_HUMAM/src/Database.java
+++ MODUL1_HUMAM/src/Database.java
@@ -1,5 +1,20 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+package MODUL1_HUMAM;
+import java.until.list;
+
+public class Database {
+
+        String InsertMenu;
+        String ShowMenu;
+        String SearchMenu;
+        public void setInsertMenu(String InsertMenu) {
+            this.InsertMenu = InsertMenu;
+
+        }
+
+        public void setShowMenu(String ShowMenu) {
+            this.ShowMenu = ShowMenu;
+        }
+
+        public void setShowMenu(String ShowMenu) {}
+            this.SearchMenu = SearchMenu;
+        }
